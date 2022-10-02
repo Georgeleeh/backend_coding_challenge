e4221d67f08bbd2e0c0a2ad66a5273a59c94d643
Georgeleeh <me+code@georgeleeh.com>
2022-10-02 20:27:28 +0100

    aded docstring and minor formatting changes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+"""
+A script for parsing and analysing sales_brand and sales_data csv files.
+Outputs a JSON file with weekly percentage growth information.
+"""
+
 def run():
     """
-    Please update this function with a function that can run your 
+    Please update this function with a function that can run your
     solution end to end, producing an output file in the output/
     directory.
     """
