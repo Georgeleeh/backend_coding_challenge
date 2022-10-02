11c3959d82632b8e70f1984577f0107147cf517e
Georgeleeh <me+code@georgeleeh.com>
2022-10-02 21:05:02 +0100

    added add_data method and tests

--- src/classes.py
+++ src/classes.py
@@ -3,6 +3,9 @@ Contains the classes/data-structures used to ingest csv data.
 """
 
 
+from datetime import date
+
+
 class WeeklyData:
     """
     Contains weekly sales information stored by period
@@ -12,3 +15,31 @@ class WeeklyData:
     def __init__(self):
         self.current: dict = None
         self.previous: dict = None
+
+    def add_data(
+        self,
+        period_id: int,
+        period_name: str,
+        week_commencement_date: date,
+        gross_sales: float,
+        units_sold: int
+    ):
+        """
+        Add current or previous data for the week.
+        """
+        period_dict = {
+            'period_id': period_id,
+            'week_commencement_date': week_commencement_date,
+            'gross_sales': gross_sales,
+            'units_sold': units_sold
+        }
+
+        if period_name == 'current':
+            self.current = period_dict
+
+        elif period_name == 'previous':
+            self.previous = period_dict
+
+        else:
+            raise ValueError(
+                "period_name not recognised. Expected 'current' or 'previous'.")
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,7 @@
 """
 Tests covering main.py and classes.py
 """
+from datetime import date
 import sys
 import os
 import pytest
@@ -11,13 +12,42 @@ sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
 
 
-def class_test_weekly_data():
+def test_class_weekly_data():
     """
     Tests for the class WeeklyData.
     """
     test_weekly_data = WeeklyData()
     assert test_weekly_data.current is None
     assert test_weekly_data.previous is None
+
+    test_weekly_data.add_data(
+        period_id=2,
+        period_name="current",
+        week_commencement_date=date(2022, 7, 4),
+        gross_sales=200,
+        units_sold=20
+    )
+
+    assert isinstance(test_weekly_data.current, dict)
+
+    test_weekly_data.add_data(
+        period_id=1,
+        period_name="previous",
+        week_commencement_date=date(2021, 7, 4),
+        gross_sales=100,
+        units_sold=10
+    )
+
+    assert isinstance(test_weekly_data.previous, dict)
+
+    with pytest.raises(ValueError):
+        test_weekly_data.add_data(
+            period_id=3,
+            period_name="next",
+            week_commencement_date=date(2023, 7, 4),
+            gross_sales=600,
+            units_sold=60
+        )
 
 
 def test_skeleton_code_returns_true():
