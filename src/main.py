bc4afd0e25ba639d8c323399ed5154f4c000dd6d
Georgeleeh <me+code@georgeleeh.com>
2022-10-02 21:10:05 +0100

    implemented ingesting sales_brand csv

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@ A script for parsing and analysing sales_brand and sales_data csv files.
 Outputs a JSON file with weekly percentage growth information.
 """
 
+import csv
+from datetime import datetime
 import os
+from pathlib import Path
 import sys
 
 sys.path.insert(0, os.path.abspath(
@@ -11,15 +14,69 @@ sys.path.insert(0, os.path.abspath(
 
 from src.classes import WeeklyData
 
+DATA_FOLDER_PATH = Path(__file__).parent.parent / Path("data")
 
-def run():
+
+def parse_sales_brand_csv(csv_filename: str) -> dict:
+    """
+    Parse a sales_brand csv file into a dictionary of WeeklyData objects.
+    This structure allows for easy filtering by brand, then week.
+    """
+
+    brands = {}
+
+    with open(csv_filename, mode='r', encoding='utf-8') as file:
+
+        csv_file = csv.DictReader(file)
+
+        for line in csv_file:
+
+            # force variable types for each line item
+            line_brand_id = int(line['brand_id'])
+            line_brand_name = str(line['brand'])
+            line_period_id = int(line['period_id'])
+            line_period_name = str(line['period_name'])
+            line_week_commencing_date = datetime.strptime(
+                line['week_commencing_date'], "%d/%m/%Y").date()
+            line_gross_sales = float(line['gross_sales'])
+            line_units_sold = int(line['units_sold'])
+
+            formatted_week_day_month = line_week_commencing_date.strftime(
+                "%d/%m"
+            )
+
+            # if the brand is new, add to the brands dict
+            if line_brand_name not in brands:
+                brands[line_brand_name] = {
+                    "brand_id": line_brand_id,
+                    "brand_name": line_brand_name,
+                    "weekly_data": {}
+                }
+
+            # if the week has no data, create a new class instance and add it to the dict
+            if formatted_week_day_month not in brands[line_brand_name]['weekly_data']:
+                brands[line_brand_name]['weekly_data'][formatted_week_day_month] = WeeklyData()
+
+            # create a WeeklyData instance for this line of data
+            brands[line_brand_name]['weekly_data'][formatted_week_day_month].add_data(
+                line_period_id,
+                line_period_name,
+                line_week_commencing_date,
+                line_gross_sales,
+                line_units_sold
+            )
+
+    return brands
+
+def run() -> bool:
     """
     Please update this function with a function that can run your
     solution end to end, producing an output file in the output/
     directory.
     """
 
-    print("Hello World")
+    sales_brand_data = parse_sales_brand_csv(
+        DATA_FOLDER_PATH / Path("sales_brand.csv"))
 
     return True
 
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,10 +2,11 @@
 Tests covering main.py and classes.py
 """
 from datetime import date
+from pathlib import Path
 import sys
 import os
 import pytest
-from src.main import run
+from src.main import run, parse_sales_brand_csv
 from src.classes import WeeklyData
 
 sys.path.insert(0, os.path.abspath(
@@ -48,6 +49,46 @@ def test_class_weekly_data():
             gross_sales=600,
             units_sold=60
         )
+
+
+def test_parse_sales_brand_csv():
+    """
+    Tests the csv ingect function for sales_brand csv files.
+    """
+    sales_brand_csv_filepath = Path(
+        __file__).parent / Path('test_sales_brand.csv')
+    assert sales_brand_csv_filepath.exists()
+    parsed_csv = parse_sales_brand_csv(sales_brand_csv_filepath.as_posix())
+
+    # check that 3 brands are present
+    assert isinstance(parsed_csv, dict)
+    assert len(parsed_csv) == 3
+
+    # check variable types are correct
+    assert isinstance(parsed_csv['Brand A'], dict)
+    assert isinstance(parsed_csv['Brand A']['brand_id'], int)
+    assert isinstance(parsed_csv['Brand A']['brand_name'], str)
+    assert isinstance(parsed_csv['Brand A']['weekly_data'], dict)
+
+    # check correct number of weekly data entries
+    assert len(parsed_csv['Brand A']['weekly_data']) == 3
+    assert len(parsed_csv['Brand B']['weekly_data']) == 1
+    assert len(parsed_csv['Brand C']['weekly_data']) == 1
+
+    # check variable types are correct
+    assert isinstance(parsed_csv['Brand A']
+                      ['weekly_data']['04/07'].current, dict)
+    assert isinstance(parsed_csv['Brand A']['weekly_data']
+                      ['04/07'].current['gross_sales'], float)
+    assert isinstance(parsed_csv['Brand A']['weekly_data']
+                      ['04/07'].current['units_sold'], int)
+
+    # check values are correct
+    assert parsed_csv['Brand A']['weekly_data']['04/07'].previous['units_sold'] == 20
+    assert parsed_csv['Brand A']['weekly_data']['04/07'].current['units_sold'] == 30
+    assert parsed_csv['Brand A']['weekly_data']['18/07'].current['units_sold'] == 40
+    assert parsed_csv['Brand B']['weekly_data']['18/07'].previous['units_sold'] == 50
+    assert parsed_csv['Brand C']['weekly_data']['25/07'].current['units_sold'] == 38
 
 
 def test_skeleton_code_returns_true():
